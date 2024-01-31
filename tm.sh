3c13711431cc1908dd369078a1411789f4cbaccc
Sergey Lezhnin <54681029+ExZuperi@users.noreply.github.com>
2024-01-31 14:56:17 +0500

    Version 1.1
    1) read -p instead of echo
    2) Other tabulation

--- tm.sh
+++ tm.sh
@@ -1,39 +1,37 @@
-#! /bin/bash
+#!/bin/bash
 
-# Sources fix
+#Sources fix
 astraVersion=$(cat /etc/astra_version)
 echo "deb http://dl.astralinux.ru/astra/frozen/1.7_x86-64/$astraVersion/repository-base/ 1.7_x86-64  main contrib non-free" > /etc/apt/sources.list
 apt update
 
-# Going to AD
+#Going to AD
 apt install fly-admin-ad-client -y
-echo "AD net: "
-read ADnet
-echo "DC name: "
-read DCname
-echo "User admin: "
-read adminName
-echo "Pass: "
-read adminPass 
-
+read -p "AD net: " ADnet
+read -p "DC name: " DCname
+read -p "User admin: " adminName
+read -p "Pass: " adminPass 
 astra-winbind -dc $DCname.$ADnet -u $adminName -p $adminPass
 # TODO: Is it required? reboot
 
-# TM preinstall
-# Package 
+#TM preinstall
+#	Package 
 apt install lsb-release lshw ntp ntpdate gsfonts libnewt0.52 libwmf-bin libwmf0.2-7 libxml2-utils python-newt -y
-# Hosts file
+
+#	Hosts file
 urIP=$(ifconfig eth0 | awk '/inet / {split($2, a, ":"); print a[1]}')
 hostname=$(hostname)
 newEntry="$urIP $hostname.$ADnet $hostname"
 sed -i "2s/.*/$newEntry/" /etc/hosts
 
-# SSH
+#	SSH
 /etc/init.d/ssh start
 systemctl enable ssh
-# Mandat
+
+#	Mandat
 pdpl-user -i 63 root
-# Folder and install
+
+#Folder and install
 mkdir /distr
 mv ./iwtm-* /distr/
 cd /distr
